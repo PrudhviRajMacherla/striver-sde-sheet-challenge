a0a3d6c3032cf831e308d5a2961d1a1792708835
PrudhviRajMacherla <macharlaprudhvi3@gmail.com>
2023-06-08 12:43:58 +0530

    Dutch_national_flag_algo_implemented

--- Sort_Array_of_0_1_2.java
+++ Sort_Array_of_0_1_2.java
@@ -1,4 +1,32 @@
 public class Sort_Array_of_0_1_2{
+    public static void swap(int[] nums,int low,int high)
+    {
+        int temp=nums[low];nums[low]=nums[high];nums[high]=temp;
+    }
+    public static void dutch_national_flag(int[] nums)
+    {
+        System.out.println("Dutch National Flag Algo");
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                //swap low and mid values
+                swap(nums,low,mid);
+                low++;mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else{
+               swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
     public static void better_solution(int[] nums) 
     {
         int ct0=0,ct1=0,ct2=0;
@@ -34,7 +62,8 @@ public class Sort_Array_of_0_1_2{
     public static void main(String[] args)
     {
         int[] arr ={2 ,1 ,2 ,0 ,1 ,0};
-        better_solution(arr);
+       // better_solution(arr);
+        dutch_national_flag(arr);
         for(int e:arr){
             System.out.print(e+"\t");
         }
